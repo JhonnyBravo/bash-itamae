298baf6c2d12cd127e0c538f707d860cf3f27dc7
Jhonny Bravo <sanfranceshika5@gmail.com>
2026-03-17 20:50:09 +0900

    modified: lib/resource/mode_resource.sh
    new function: get_mode, normalize_mode

--- lib/resource/mode_resource.sh
+++ lib/resource/mode_resource.sh
@@ -1,15 +1,25 @@
+get_mode() {
+	local target="$RESOURCE_PATH"
+	stat -c %a "$target"
+}
+
+normalize_mode() {
+	printf "%o" "$((8#$1))"
+}
+
 update_mode() {
-	local target="$TARGET"
+	local target="$RESOURCE_PATH"
 
 	# 現在のパーミッション設定取得
-	local current_mode
-	current_mode=$(stat -c %a "$target")
+	local current_mode=$(get_mode)
+	local normalized_cur_mode="$(normalize_mode "$current_mode")"
+	local normalized_new_mode="$(normalize_mode "$RESOURCE_MODE")"
 
 	# 同一なら何もしない
-	if [ "$current_mode" = "$MODE" ]; then
+	if [ "$normalized_cur_mode" = "$normalized_new_mode" ]; then
 		return 0
 	fi
 
-	chmod "$MODE" "$target"
+	chmod "$RESOURCE_MODE" "$target"
 	return 2
 }
