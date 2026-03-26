e98626e7c047770035509e89b55c235f594a46d1
Jhonny Bravo <sanfranceshika5@gmail.com>
2026-03-26 20:28:01 +0900

    ログ出力処理を追加
    modified: lib/resource/owner_resource.sh
    modified: test/owner_resource/update.bats

--- lib/resource/owner_resource.sh
+++ lib/resource/owner_resource.sh
@@ -1,3 +1,7 @@
+if ! type info >/dev/null 2>&1; then
+	source "$(dirname "$BASH_SOURCE[0]")/log_resource.sh"
+fi
+
 get_owner() {
 	local target="$RESOURCE_PATH"
 	stat -c %U "$target"
@@ -8,12 +12,14 @@ update_owner() {
 
 	# 現在の所有者取得
 	local current_owner=$(get_owner)
+	local new_owner="$RESOURCE_OWNER"
 
 	# 同一なら何もしない
-	if [ "$current_owner" = "$RESOURCE_OWNER" ]; then
+	if [ "$current_owner" = "$new_owner" ]; then
 		return 0
 	fi
 
-	chown "$RESOURCE_OWNER" "$target"
+	info "Changing owner from ${current_owner} to ${new_owner}"
+	chown "$new_owner" "$target"
 	return 2
 }
--- test/owner_resource/update.bats
+++ test/owner_resource/update.bats
@@ -16,6 +16,7 @@ teardown() {
 
 @test "update_owner 実行時に RESOURCE_OWNER に指定された所有者と現在の所有者が異なる場合は、所有者を変更して exit 2 を返す" {
 	run update_owner
+	echo "$output" >&3
 	[ "$status" -eq 2 ]
 
 	# chown モック化の副作用で所有者変更のテストができない為、対象外とする。
@@ -26,6 +27,7 @@ teardown() {
 @test "update_owner 実行時に RESOURCE_OWNER に指定された所有者と現在の所有者が一致する場合は、所有者を変更せずに exit 0 を返す" {
 	source ./test/owner_resource/cases/same.env
 	run update_owner
+	echo "$output" >&3
 	[ "$status" -eq 0 ]
 
 	local new_owner=$(get_owner)
